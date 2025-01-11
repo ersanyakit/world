195492832b8e9819a518970a11b4777590bd2c32
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-01-11 14:53:12 +0700

    fixes

--- src/components/Map/ui/DrawerPanel/Tabs/Profile.tsx
+++ src/components/Map/ui/DrawerPanel/Tabs/Profile.tsx
@@ -45,93 +45,89 @@ export const ProfileTAB = () => {
     }
 
     return (
-        <div className="flex flex-col gap-2">
-            <div className="w-full">
-                <Tabs aria-label="Tabs colors" variant="light" fullWidth color={"primary"}>
-                    <Tab key="account" title="Account">
-                        {
-                            isConnected ? (
-                                player ? (
-                                    player.registered ? (
-                                        <AccountTAB />
-                                    ) : (
-                                        <RegisterTAB />
-                                    )
-                                ) : (
-                                    <RegisterTAB />
-                                )
-                            ) : (
-                                <ConnectTab />
-                            )
-                        }
-                    </Tab>
-                    <Tab key="referrals" title="Referrals">
-                        <ScrollShadow hideScrollBar>
-                            <div className="w-full flex flex-col gap-2">
+        <Tabs classNames={{tabContent:"!text-white" }} aria-label="Tabs colors" variant="light" fullWidth color={"primary"}>
+        <Tab key="account" title="Account">
+            {
+                isConnected ? (
+                    player ? (
+                        player.registered ? (
+                            <AccountTAB />
+                        ) : (
+                            <RegisterTAB />
+                        )
+                    ) : (
+                        <RegisterTAB />
+                    )
+                ) : (
+                    <ConnectTab />
+                )
+            }
+        </Tab>
+        <Tab key="referrals" title="Referrals">
+            <ScrollShadow hideScrollBar>
+                <div className="w-full flex flex-col gap-2">
 
 
-                                {
-                                    player?.referrals.length === 0 ? (
-                                        <NoItemAvailable
-                                            icon="/assets/referrals.png"
-                                            title="No referrals yet."
-                                            description="Invite others to join and grow your network. Each referral brings you closer to financial success!"
-                                        />
-                                    ) : (
-                                        player?.referrals.slice().reverse().map((referral, index) => (
-                                            <ReferralCard key={index} referral={referral} />
-                                        ))
-                                    )
-                                }
+                    {
+                        player?.referrals.length === 0 ? (
+                            <NoItemAvailable
+                                icon="/assets/referrals.png"
+                                title="No referrals yet."
+                                description="Invite others to join and grow your network. Each referral brings you closer to financial success!"
+                            />
+                        ) : (
+                            player?.referrals.slice().reverse().map((referral, index) => (
+                                <ReferralCard key={index} referral={referral} />
+                            ))
+                        )
+                    }
 
 
-                            </div>
-                        </ScrollShadow>
-                    </Tab>
-                    <Tab key="followers" title="Followers">
-                        <ScrollShadow hideScrollBar>
-                            <div className="w-full flex flex-col gap-2">
-                                {
-                                    player?.followers.length === 0 ? (
-                                        <NoItemAvailable
-                                            icon="/assets/followers.png"
-                                            title="You are not following anyone."
-                                            description="Follow users to accelerate your journey to becoming a millionaire!"
-                                        />
-                                    ) : (
-                                        player?.followers.slice().reverse().map((follower, index) => (
-                                            <ReferralCard key={index} referral={follower} />
-                                        ))
-                                    )
-                                }
-                            </div>
-                        </ScrollShadow>
-                    </Tab>
-                    <Tab key="followings" title="Followings">
-                        <ScrollShadow hideScrollBar>
-                            <div className="w-full flex flex-col gap-2">
+                </div>
+            </ScrollShadow>
+        </Tab>
+        <Tab key="followers" title="Followers">
+            <ScrollShadow hideScrollBar>
+                <div className="w-full flex flex-col gap-2">
+                    {
+                        player?.followers.length === 0 ? (
+                            <NoItemAvailable
+                                icon="/assets/followers.png"
+                                title="You are not following anyone."
+                                description="Follow users to accelerate your journey to becoming a millionaire!"
+                            />
+                        ) : (
+                            player?.followers.slice().reverse().map((follower, index) => (
+                                <ReferralCard key={index} referral={follower} />
+                            ))
+                        )
+                    }
+                </div>
+            </ScrollShadow>
+        </Tab>
+        <Tab key="followings" title="Followings">
+            <ScrollShadow hideScrollBar>
+                <div className="w-full flex flex-col gap-2">
 
-                                {
-                                    player?.followings.length === 0 ? (
-                                        <NoItemAvailable
-                                            icon="/assets/following.png"
-                                            title="You haven't followed anyone yet."
-                                            description="Start following people to expand your network and unlock more opportunities!"
-                                        />
-                                    ) : (
-                                        player?.followings.slice().reverse().map((following, index) => (
-                                            <ReferralCard key={index} referral={following} />
-                                        ))
-                                    )
-                                }
-                            </div>
+                    {
+                        player?.followings.length === 0 ? (
+                            <NoItemAvailable
+                                icon="/assets/following.png"
+                                title="You haven't followed anyone yet."
+                                description="Start following people to expand your network and unlock more opportunities!"
+                            />
+                        ) : (
+                            player?.followings.slice().reverse().map((following, index) => (
+                                <ReferralCard key={index} referral={following} />
+                            ))
+                        )
+                    }
+                </div>
 
 
-                        </ScrollShadow>
-                    </Tab>
+            </ScrollShadow>
+        </Tab>
 
-                </Tabs>
-            </div>
-        </div>
+    </Tabs>
     )
 }
